c8d37cd192073b3b72d679e530829a3185eefd05
Riya Gharat <riya.gharat@yahoo.com>
2017-03-29 14:11:52 -0400

    Added some methods

--- TSQLx.java
+++ TSQLx.java
@@ -3,4 +3,23 @@ public class TSQLx{
   public static void main(String [] args){
     
   }
+  public static void createDatabase(String dataBaseName){
+  
+  }
+
+  public static void dropDatabase(String dataBaseName){
+    
+  }
+
+  public static void saveDatabase(String dataBaseName){
+    
+  }
+
+  public static void loadDatabase(String dataBaseName){
+    
+  }
+
+  public static void createTable(){
+    
+  }
 }
