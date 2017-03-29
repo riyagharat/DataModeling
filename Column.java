0bf10db5868d7377e5c2885d1a18c11e73364a00
spottedcanadian <nckross1000@gmail.com>
2017-03-29 14:09:36 -0400

    Update Column.java

--- Column.java
+++ Column.java
@@ -3,33 +3,22 @@ class Column{
   private List<Row> list;
   private Column nextColumn;
   private String name;
-  private Boolean notNull;
-  private Column dateTime;
 
   public Column(){
 
   }
 
-  public Column(List<Row> list, String name, Boolean notNull) {
+  public Column(List<Row> list, String name) {
     this.list = list;
     this.name = name;
-    this.notNull = notNull;
   }
 
   public String getName(){
     return name;
   }
 
-  public Boolean getNotNull(){
-    return notNull;
-  }
-
   public void setName(String newName){
     this.name = newName;
-  }
-
-  public void setNotNull(Boolean newNotNull){
-    this.notNull = newNotNull;
   }
 
   public boolean equals(Object obj){
