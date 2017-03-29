b88e09de8ce43f8d675ca669da6e1d42650a660e
Riya Gharat <riya.gharat@yahoo.com>
2017-03-29 14:38:21 -0400

    Added more functions

--- TSQLx.java
+++ TSQLx.java
@@ -22,4 +22,52 @@ public class TSQLx{
   public static void createTable(){
     
   }
+  
+  public static void dropDatabase(String dataBaseName){
+
+  }
+
+  public static void saveDatabase(String dataBaseName){
+
+  }
+
+  public static void loadDatabase(String dataBaseName){
+
+  }
+
+  public static void createTable(){
+
+  }
+
+  public static void dropTable(String tableName){
+
+  }
+
+  public static void SQLInsert(){
+
+  }
+
+  public static void convertXML(){
+
+  }
+
+  public static void inputFile(){
+
+  }
+
+  public static void deleteFromTable(){
+
+  }
+
+  public static void simpleSelect(){
+
+  }
+
+  public static void dateSelect(){
+
+  }
+
+  public static void dateSelectComplicated(){
+    
+  }
 }
