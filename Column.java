75ccfaa62169455a987e7c8ad9c3e100cf224152
bfeelin <brianphelan60@gmail.com>
2017-03-31 22:09:36 -0400

    Update Column.java

--- Column.java
+++ Column.java
@@ -5,6 +5,7 @@ class Column{
   private String name;
   private String dataType;
   private Column dateTime;
+private Boolean notNull;
 
   public Column(){
 
@@ -31,4 +32,16 @@ class Column{
   public String toString(){
       return getName() + " " + getNotNull() + "\n";
   }
+
+  	public Boolean getNotNull() {
+	return notNull;
+	}
+
+	public Column getNextColumn() {
+	return nextColumn;
+	}
+
+	public void setNextColumn(Column nextColumn) {
+		this.nextColumn = nextColumn;
+	}
 }
