c6075445cf733b7486552a7712a71bee2bc184f5
bfeelin <brianphelan60@gmail.com>
2017-04-10 19:39:42 -0400

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,19 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 // Table Class
 
 class Table{
-  private List<Column> list;
-  private Table nextTable;
-  private String name;
+	 ArrayList<Column> list = new ArrayList<Column>();
+	  private Table nextTable;
+	  private String name;
+	  public int deletionIndex;
+
 
   public Table(){
 
   }
 
-  public Table(List<Column> list, String name) {
-    this.list = list;
-    this.name = name;
-  }
 
+  public Table(String[] columnNames, String name) 
+  {
+	  Column prevCol = null;
+    for(int i = 0; i < columnNames.length; i++)
+    {
+    	Column newCol = new Column();
+    	newCol.setName(columnNames[i]);
+    	this.list.add(newCol);
+    	if(i > 0)
+    		prevCol.setNextColumn(newCol);
+    	prevCol = newCol;
+    }
+    this.name = name;
+    System.out.println("Table " + this.name + " created with attributes " + Arrays.toString(columnNames));
+}
   public String getName(){
     return name;
   }
@@ -28,5 +47,42 @@ class Table{
 
   public String toString(){
       return getName() + "\n";
+  }
+  
+  public void insert(ArrayList<String> fields, ArrayList<String> values)
+  {	  
+	  for(int i = 0; i < this.list.size(); i ++)			//create row objects
+	  {
+		  Column prevCol;
+		 
+		  if(i == 0)
+			  prevCol = null;
+		  else prevCol = list.get(i - 1);
+		  
+		  list.get(i).initializeRow(prevCol);
+		  if(i == list.size() - 1)
+		  {
+			  Column firstCol = list.get(0);
+			  list.get(i).setCircPtr(firstCol);
+	      }
+	
+	  }
+	  
+	  if(fields.isEmpty())
+	  {
+		  for(int i = 0; i < values.size(); i ++)
+			  list.get(i).insertRecord(values.get(i));  			//simple insert
+	  }
+	  else
+	  {
+		  for(int i = 0; i < values.size(); i ++)						//search for correct column name insert
+		  {
+			  int colIndex = 0;
+			  while(colIndex < this.list.size() - 1 && !(this.list.get(colIndex).getName().equals(fields.get(i))))		//while not correct column	  
+				  colIndex++;
+		  
+			  list.get(colIndex).insertRecord(values.get(i));  
+	    	}
+	  }
   }
 }
