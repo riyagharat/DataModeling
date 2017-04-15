891d4fdf1001924071d0d83d527851f8098ffc6f
Jawbreak1r <Chance1337@Yahoo.com>
2017-04-15 14:11:22 -0400

    Fixing Syntax Errors
    Fixing Syntax Errors

--- TSQLx.java
+++ TSQLx.java
@@ -1,22 +1,28 @@
 // TSQLx.java
+import java.util.ArrayList;
+import java.io.File;
+
 public class TSQLx{
+   
+  static ArrayList listOfTables = new ArrayList<Table>();
+  
   public static void main(String [] args){
     
   }
   
-  public static void createDatabase(String dataBaseName){
+  /*public static void createDatabase(String dataBaseName){
     File dataBaseName = new File();
-    if(dataBaseName.exists(){
+    if(dataBaseName.exists()){
       System.out.println("Please stoperino before you make a duplicate filerino");
     }else dataBaseName.createNewFile();
   }
 
   public static void dropDatabase(String dataBaseName){
-    if(dataBaseName.exists(){
+    if(dataBaseName.exists()){
         dataBaseName.delete();
         System.out.println(dataBaseName + " has been deleted");
     }else System.out.println("That doesn't exist my guy");    
-  }
+  }*/
 
   public static void saveDatabase(String dataBaseName){
     
@@ -30,55 +36,54 @@ public class TSQLx{
     
   }
        
-  public static void simpleSelect(Arraylist<String> Printlist, Arraylist<String> TableNamer, Arraylist<String> Wheres){
-   
+  public static void simpleSelect(ArrayList<String> Printlist, ArrayList<String> TableNamer, ArrayList<String> Wheres){
    String TableName = TableNamer.get(0);
    int TableIndex = 0;
    String ColumnName;
    String Relational;
    String Comparator;
    int Case;
-   if(Wheres.size > 0){
-      String ColumnName = Wheres.get(0);
-      String Relational = Wheres.get(1);
-      String Comparator = Wheres.get(2);
+   if(Wheres.size() > 0){
+      ColumnName = Wheres.get(0);
+      Relational = Wheres.get(1);
+      Comparator = Wheres.get(2);
    }
    
-   if(Wheres.size > 0){
-      if(Relational.equals("<")
+   if(Wheres.size() > 0){
+      if(Relational.equals("<"))
          Case = 1;
-      if(Relational.equals(">")
+      if(Relational.equals(">"))
          Case = 2;
-      if(Relational.equals("=")
+      if(Relational.equals("="))
          Case = 3;
-      if(Relational.equals("<>")
+      if(Relational.equals("<>"))
          Case = 4;
-      if(Relational.equals("<=")
+      if(Relational.equals("<="))
          Case = 5;
-      if(Relational.equals(">=")
+      if(Relational.equals(">="))
          Case = 6;
    }               
    
    while(!listOfTables.get(TableIndex).equals(TableName)){//Is this table a thing?
       TableIndex++;
-      if(TableIndex = listOfTables.size()){//If you've gone too far, give up
+      if(TableIndex == listOfTables.size()){//If you've gone too far, give up
          System.out.println("That table doesn't exist ya silly goose");
          return;
       }
    }
    
-   if(Wheres.size = 0){
+   if(Wheres.size() == 0){
       if(Printlist.get(0).equals("*")){
          printEverything(TableIndex);
          return;
       }else{
-         Arraylist indices = new Arraylist<Integer>();
+         ArrayList indices = new ArrayList<Integer>();
          int checking = 0;
-         for(int k = 0; k < Printlist.size()){
+         for(int k = 0; k < Printlist.size(); k++){
             int Persona = 0;
-            while(!listOfTables.get(TableIndex).list.get(i).equals(Wheres.get(Persona))){
+            while(!listOfTables.get(TableIndex).list.get(k).equals(Wheres.get(Persona))){
                Persona++;
-               if(Persona = listOfTables.get(TableIndex).list.size()){
+               if(Persona == listOfTables.get(TableIndex).list.size()){
                   System.out.println("Excuse me, WHAT column?");
                   return;
                }
@@ -89,10 +94,11 @@ public class TSQLx{
          printSomething(TableIndex, indices);
       }//end else
    }else{//OH SHIT BOIS IT'S A WHERE STATEMENT HOLD ON TO YOUR BUTTS
-      Arraylist ShitWeNeed = new Arraylist<Integer>();
+      ArrayList ShitWeNeed = new ArrayList<Integer>();
+      int killMe = 0;
       switch(Case){
          case 1://less than? the world may never know
-            int killMe = 0;
+            killMe = 0;
             while(!listOfTables.get(TableIndex).list.get(killMe).equals(ColumnName))
                killMe++;
             for(int monsterMash = 0; listOfTables.get(TableIndex).list.get(killMe).list.size(); monsterMash++){
@@ -100,14 +106,14 @@ public class TSQLx{
                   System.out.println("Yeah man, String less than whatever, great idea, please choke on a scooter");
                   return;
                }else{
-                  int compareMeDaddy = Comparator.parseInt();
                   if(listOfTables.get(TableIndex).list.get(killMe).list.get(monsterMash).data().compareTo(Comparator) > 1){
                      ShitWeNeed.add(monsterMash);
                   }
                }
             }
+            break;
          case 2://greater than? check later
-            int killMe = 0;
+            killMe = 0;
             while(!listOfTables.get(TableIndex).list.get(killMe).equals(ColumnName))
                killMe++;
             for(int monsterMash = 0; listOfTables.get(TableIndex).list.get(killMe).list.size(); monsterMash++){
@@ -115,7 +121,6 @@ public class TSQLx{
                   System.out.println("Yeah man, String less than whatever, great idea, please choke on a scooter");
                   return;
                }else{
-                  int compareMeDaddy = Comparator.parseInt();
                   if(listOfTables.get(TableIndex).list.get(killMe).list.get(monsterMash).data().compareTo(Comparator) < 1){
                      ShitWeNeed.add(monsterMash);
                   }
@@ -123,7 +128,7 @@ public class TSQLx{
             } 
             break;
          case 3://equal to
-            int killMe = 0;
+            killMe = 0;
             while(!listOfTables.get(TableIndex).list.get(killMe).equals(ColumnName))
                killMe++;
             for(int monsterMash = 0; listOfTables.get(TableIndex).list.get(killMe).list.size(); monsterMash++){
@@ -133,7 +138,7 @@ public class TSQLx{
             } 
             break;
          case 4://not equal   
-            int killMe = 0;
+            killMe = 0;
             while(!listOfTables.get(TableIndex).list.get(killMe).equals(ColumnName))
                killMe++;
             for(int monsterMash = 0; listOfTables.get(TableIndex).list.get(killMe).list.size(); monsterMash++){
@@ -143,7 +148,7 @@ public class TSQLx{
             } 
             break;
          case 5://less than or equal to?
-            int killMe = 0;
+            killMe = 0;
             while(!listOfTables.get(TableIndex).list.get(killMe).equals(ColumnName))
                killMe++;
             for(int monsterMash = 0; listOfTables.get(TableIndex).list.get(killMe).list.size(); monsterMash++){
@@ -151,7 +156,6 @@ public class TSQLx{
                   System.out.println("Yeah man, String less than whatever, great idea, please choke on a scooter");
                   return;
                }else{
-                  int compareMeDaddy = Comparator.parseInt();
                   if(listOfTables.get(TableIndex).list.get(killMe).list.get(monsterMash).data().compareTo(Comparator) < 1 || 
                      listOfTables.get(TableIndex).list.get(killMe).list.get(monsterMash).data().equals(Comparator)){
                      ShitWeNeed.add(monsterMash);
@@ -160,7 +164,7 @@ public class TSQLx{
             } 
             break;
          case 6://greater than or equal to?
-            int killMe = 0;
+            killMe = 0;
             while(!listOfTables.get(TableIndex).list.get(killMe).equals(ColumnName))
                killMe++;
             for(int monsterMash = 0; listOfTables.get(TableIndex).list.get(killMe).list.size(); monsterMash++){
@@ -168,7 +172,6 @@ public class TSQLx{
                   System.out.println("Yeah man, String less than whatever, great idea, please choke on a scooter");
                   return;
                }else{
-                  int compareMeDaddy = Comparator.parseInt();
                   if(listOfTables.get(TableIndex).list.get(killMe).list.get(monsterMash).data().compareTo(Comparator) > 1 || 
                      listOfTables.get(TableIndex).list.get(killMe).list.get(monsterMash).data().equals(Comparator)){
                      ShitWeNeed.add(monsterMash);
@@ -181,13 +184,13 @@ public class TSQLx{
          please(TableIndex, ShitWeNeed);
          return;
       }else{
-         Arraylist indices = new Arraylist<Integer>();
+         ArrayList indices = new ArrayList<Integer>();
          int checking = 0;
-         for(int k = 0; k < Printlist.size()){
+         for(int k = 0; k < Printlist.size(); k++){
             int Persona = 0;
-            while(!listOfTables.get(TableIndex).list.get(i).equals(Wheres.get(Persona))){
+            while(!listOfTables.get(TableIndex).list.get(k).equals(Wheres.get(Persona))){
                Persona++;
-               if(Persona = listOfTables.get(TableIndex).list.size()){
+               if(Persona == listOfTables.get(TableIndex).list.size()){
                   System.out.println("Excuse me, WHAT column?");
                   return;
                }
@@ -200,7 +203,7 @@ public class TSQLx{
    }   
 }
 
-public static void please(int TableIndex, Arraylist helpMe){
+public static void please(int TableIndex, ArrayList helpMe){
    //remind me to put the names of the columns up here some time
    for(int i = 0; i < listOfTables.get(TableIndex).list.size(); i++){//this for loop's checking for number of records
       for(int j = 0; j < listOfTables.get(TableIndex).list.get(i).list.get(j); i++){//and this loop is getting those records
@@ -216,7 +219,7 @@ public static void please(int TableIndex, Arraylist helpMe){
    }
 }
 
-public static void AbbassiWhy(int TableIndex, Arraylist helpMe){
+public static void AbbassiWhy(int TableIndex, ArrayList helpMe){
 //remind me to put the names of the columns up here some time
    for(int i = 0; i < listOfTables.get(TableIndex).list.size(); i++){//this for loop's checking for column size
       if(indices.contains(i)){
@@ -249,7 +252,7 @@ public static void printEverything(int TableIndex){
    }
 }
 
-public static void printSomething(int TableIndex, Arraylist<Integer> indices){
+public static void printSomething(int TableIndex, ArrayList<Integer> indices){
    //remind me to put the names of the columns up here some time
    for(int i = 0; i < listOfTables.get(TableIndex).list.size(); i++){//this for loop's checking for column size
       if(indices.contains(i)){
