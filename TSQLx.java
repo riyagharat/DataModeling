e8e63258475493a0b35be95b943932e651ad04ed
Riya Gharat <riya.gharat@yahoo.com>
2017-04-15 13:49:34 -0400

    Updated TSQLx.java

--- TSQLx.java
+++ TSQLx.java
@@ -3,15 +3,6 @@ public class TSQLx{
   public static void main(String [] args){
     
   }
-  public static void createDatabase(String dataBaseName){
-    File file = dataBaseName + ".txt";
-    file.open();
-  }
-
-  public static void dropDatabase(String dataBaseName){
-    File file = dataBaseName + ".txt";
-    file.delete();
-  }
 
   public static void saveDatabase(String dataBaseName){
     
@@ -21,17 +12,18 @@ public class TSQLx{
     
   }
 
-  public static void createTable(){
+  public static void create(){
     // need to have the columns set up here
     Table newTable = (tableName, "", "");
     listOfTables.add(newTable);
   }
 
-  public static void createTable(){
-
-  }
-
-  public static void dropTable(String tableName){
+  public static void drop(String tableName){
+    // drop database
+    File file = dataBaseName + ".txt";
+    file.delete();
+    
+    // drop tables
     for(int i = 0; i < listOfTables.size()){
       if((listOfTables.get(i).getName()).equals(tableName)){
         listOfTables.get(i).remove();
@@ -39,8 +31,12 @@ public class TSQLx{
     }
   }
 
-  public static void SQLInsert(){
+  public static void insert(){
 
+  }
+  
+  public static void delete(){
+    
   }
 
   public static void convertXML(){
@@ -51,19 +47,11 @@ public class TSQLx{
 
   }
 
-  public static void deleteFromTable(){
-
-  }
-
-  public static void simpleSelect(){
+  public static void select(){
 
   }
 
   public static void dateSelect(){
 
-  }
-
-  public static void dateSelectComplicated(){
-    
   }
 }
