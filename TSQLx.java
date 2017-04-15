53aa55e6f2a26eda7146647673e6de93e62c5b9b
TannerM504 <N00935492@ospreys.unf.edu>
2017-04-15 12:09:52 -0400

    Update TSQLx.java

--- TSQLx.java
+++ TSQLx.java
@@ -3,14 +3,19 @@ public class TSQLx{
   public static void main(String [] args){
     
   }
+  
   public static void createDatabase(String dataBaseName){
-    File file = dataBaseName + ".txt";
-    file.open();
+    File dataBaseName = new File();
+    if(dataBaseName.exists(){
+      System.out.println("Please stoperino before you make a duplicate filerino");
+    }else dataBaseName.createNewFile();
   }
 
   public static void dropDatabase(String dataBaseName){
-    File file = dataBaseName + ".txt";
-    file.delete();
+    if(dataBaseName.exists(){
+        dataBaseName.delete();
+        System.out.println(dataBaseName + " has been deleted");
+    }else System.out.println("That doesn't exist my guy");    
   }
 
   public static void saveDatabase(String dataBaseName){
@@ -22,48 +27,81 @@ public class TSQLx{
   }
 
   public static void createTable(){
-    // need to have the columns set up here
-    Table newTable = (tableName, "", "");
-    listOfTables.add(newTable);
-  }
-
-  public static void createTable(){
-
-  }
-
-  public static void dropTable(String tableName){
-    for(int i = 0; i < listOfTables.size()){
-      if((listOfTables.get(i).getName()).equals(tableName)){
-        listOfTables.get(i).remove();
-      }
-    }
-  }
-
-  public static void SQLInsert(){
-
-  }
-
-  public static void convertXML(){
-
-  }
-
-  public static void inputFile(){
-
-  }
-
-  public static void deleteFromTable(){
-
-  }
-
-  public static void simpleSelect(){
-
-  }
-
-  public static void dateSelect(){
-
-  }
-
-  public static void dateSelectComplicated(){
     
   }
+       
+  public static void simpleSelect(Arraylist<String> Printlist, String TableName, Arraylist<String> Wheres){
+   
+   int TableIndex = 0;
+   String ColumnName;
+   String Relational;
+   String Comparator;
+   if(Wheres.size > 0){
+      String ColumnName = Wheres.get(0);
+      String Relational = Wheres.get(1);
+      String Comparator = Wheres.get(2);
+   }
+   
+   while(listOfTables.get(TableIndex) != TableName){//Is this table a thing?
+      TableIndex++;
+      if(TableIndex = listOfTables.size()){//If you've gone too far, give up
+         System.out.println("That table doesn't exist ya silly goose");
+         return;
+      }
+   }
+   
+   if(Wheres.size = 0){
+      if(Printlist.get(0) = "*"){
+         printEverything(TableIndex);
+         return;
+      }else{
+         Arraylist indices = new Arraylist<int>;
+         int checking = 0;
+         for(int k = 0; k < Wheres.size()){
+            int Persona = 0;
+            while(listOfTables.get(TableIndex).list.get(i) != Wheres.get(Persona)){
+               Persona++;
+               if(Persona = listOfTables.get(TableIndex).list.size()){
+                  System.out.println("Excuse me, WHAT column?");
+                  return;
+               }
+            indices.add(Persona);   
+            }//end while
+         }//end forloop
+         
+      }//end else
+   }//end if statement
+}
+
+public static void printEverything(int TableIndex){
+   //remind me to put the names of the columns up here some time
+   for(int i = 0; i < listOfTables.get(TableIndex).list.size(); i++){//this for loop's checking for number of records
+      for(int j = 0; j < listOfTables.get(TableIndex).list.get(i).list.get(j); i++){//and this loop is getting those records
+         if(listOfTables.get(TableIndex).list.get(i).list.get(j).data = null){//just tab twice for empty
+            System.out.print("\t\t");
+         }else{
+            System.out.print(listOfTables.get(TableIndex).list.get(i).list.get(j).data + "\t");
+         }   
+      }
+      System.out.print("\n");
+   }
+}
+
+public static void printSomething(int TableIndex, Arraylist<int> indices){
+   //remind me to put the names of the columns up here some time
+   for(int i = 0; i < listOfTables.get(TableIndex).list.size(); i++){//this for loop's checking for column size
+      if(indices.contains(i)){
+         for(int j = 0; j < listOfTables.get(TableIndex).list.get(i).list.get(j); i++){//and here we got some rows
+               if(listOfTables.get(TableIndex).list.get(i).list.get(j).data = null){//just tab twice for rows
+                  System.out.print("\t\t");
+               }else{
+                  System.out.print(listOfTables.get(TableIndex).list.get(i).list.get(j).data + "\t");
+               }   
+         }
+         System.out.print("\n");
+      }
+   }
+}    
+       
+       
 }
