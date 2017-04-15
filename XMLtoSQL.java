715b05cc998a1913bce86fec3433e5c8fc7da6b5
Riya Gharat <riya.gharat@yahoo.com>
2017-04-15 15:19:42 -0400

    Update XMLtoSQL
    Need: Table Creation
    Finished: inputfile generation with commands

--- XMLtoSQL.java
+++ XMLtoSQL.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class XMLtoSQL{
   public static void main(String [] args){
-    if(args.length == 2){
+    if(args.length == 3){
       File XSDfile = new File(args[1]);
       File XMLfile = new File(args[0]);
       String inputFileName = new String(args[2]);
@@ -16,7 +16,6 @@ public class XMLtoSQL{
   }
 
   public static void bothFiles(File XSDfile, File XMLfile, String inputFileName){
-    File inputFileName = new File(inputFileName + ".txt");
     ArrayList<XSDFormat> XSDList = new ArrayList<XSDFormat>();
     // need to add the XSD stuff to an array List
     // Would check the XML format against this
@@ -26,7 +25,6 @@ public class XMLtoSQL{
       while(input.hasNextLine()){
         String text = input.nextLine();
         if(text.contains("xsd:complexType name=")){
-    //      System.out.println(text.indexOf("="));
           String tablename = "";
           for(int i = (text.indexOf("=") +2); i < text.length(); i++){
             if(!(text.charAt(i) == ('\"'))){
@@ -35,13 +33,11 @@ public class XMLtoSQL{
               break;
             }
           }
-//          System.out.println("Table Name/ Object Name: " + tablename);
           XSDFormat format = new XSDFormat(tablename, "", "", "", "", "");
           XSDList.add(format);
           System.out.println("");
         }else if(text.contains("xsd:element name=")){
           int firstEqualIndex = text.indexOf("=");
-    //      System.out.println(firstEqualIndex);
           String attributeName = "";
           for(int i = firstEqualIndex+2; i < text.length(); i++){
             if(!(text.charAt(i) == ('\"'))){
@@ -51,12 +47,10 @@ public class XMLtoSQL{
             }
           }
           System.out.println("");
-    //      System.out.println("Attribute Name: " + attributeName);
           XSDFormat format2 = new XSDFormat("", attributeName, "", "", "", "");
           int colonIndex = 0;
           if(text.contains("type=")){
             colonIndex = text.indexOf(":", firstEqualIndex);
-  //          System.out.println(colonIndex);
             String attributeType = "";
             for(int i = colonIndex+1; i < text.length(); i++){
               if(!(text.charAt(i) == ('\"'))){
@@ -69,13 +63,11 @@ public class XMLtoSQL{
               attributeType = "char";
             }
             attributeType = attributeType.toUpperCase();
-    //        System.out.println("Attribute Type: " + attributeType);
             format2.setAttributeType(attributeType);
           }
           int index = 0;
           if(text.contains("maxOccurs=")){
             index = text.indexOf("=", colonIndex);
-  //          System.out.println(index);
             String attributeLength = "";
             for(int i = index+2; i < text.length(); i++){
               if(!(text.charAt(i) == ('\"'))){
@@ -84,13 +76,11 @@ public class XMLtoSQL{
                 break;
               }
             }
-  //          System.out.println("Attribute Length: " + attributeLength);
             format2.setAttributeLength(attributeLength);
           }
           int fractionIndex = 0;
           if(text.contains("fraction=")){
             fractionIndex = text.lastIndexOf("=");
-  //          System.out.println(fractionIndex);
             String decimals = "";
             for(int i = fractionIndex+2; i < text.length(); i++){
               if(!(text.charAt(i) == ('\"'))){
@@ -99,13 +89,11 @@ public class XMLtoSQL{
                 break;
               }
             }
-  //          System.out.println("Decimal Places: " + decimals);
             format2.setFraction(decimals);
           }
           int dateIndex = 0;
           if(text.contains("date=")){
             dateIndex = text.lastIndexOf("=");
-  //          System.out.println(dateIndex);
             String dateFormat = "";
             for(int i = dateIndex+2; i < text.length(); i++){
               if(!(text.charAt(i) == ('\"'))){
@@ -114,7 +102,6 @@ public class XMLtoSQL{
                 break;
               }
             }
-  //          System.out.println("Date Format: " + dateFormat);
             format2.setDateFormat(dateFormat);
           }
           XSDList.add(format2);
@@ -131,12 +118,104 @@ public class XMLtoSQL{
       int counter = 0;
       String attributes = "";
       String values = "";
-      while(input2.hasNextLine()){
-        String text = input2.nextLine();
+      PrintStream fileStream = new PrintStream(new FileOutputStream(inputFileName + ".txt", true));
+        while(input2.hasNextLine()){
+          String text = input2.nextLine();
+          if(text.contains("<")){
+            String tablename = "";
+            String attributename = "";
+            String value = "";
+            int newIndex = 0;
+            for(int i = (text.indexOf("<") +1); i < text.length(); i++){
+              if((!(text.charAt(i) == ('>'))) && (!(text.charAt(i) == ('?')))){
+                if(!(text.charAt(i) == ('/'))){
+                  tablename = tablename + text.charAt(i);
+                }else{
+                  check = false;
+                  break;
+                }
+              }else if((text.indexOf(">") + 1) < text.length()){
+                if((((text.charAt(text.indexOf(">") + 1)) >= 'a') && ((text.charAt(text.indexOf(">") + 1)) <= 'z'))
+                    || (((text.charAt(text.indexOf(">") + 1)) >= 'A') && ((text.charAt(text.indexOf(">") + 1)) <= 'Z'))
+                    || (((text.charAt(text.indexOf(">") + 1)) >= '0') && ((text.charAt(text.indexOf(">") + 1)) <= '9'))){
+                  attributename = tablename;
+                  tablename = "";
+                  for(int j = (text.indexOf(">") +1); j < text.length(); j++){
+                    if((!(text.charAt(j) == ('<')))){
+                      value = value + text.charAt(j);
+                    }else{
+                      break;
+                    }
+                  }
+                }
+                check = true;
+                break;
+              }else{
+                break;
+              }
+            }
+            if(check == true){
+              counter++;
+              attributeNames.add(attributename);
+              attributeValues.add(value);
+            }else{
+              if(attributes.length() > 0){
+                attributes = attributes.substring(1, attributes.length()-1);
+              }
+
+              if(values.length() > 0){
+                values = values.substring(1, values.length()-1);
+              }
+              if(!(attributes.equals(""))){
+                fileStream.println("INSERT INTO " + XSDList.get(0).getTableName() + " (" + attributes
+                    + ") VALUES (" + values + ");");
+              }
+
+              counter = 0;
+              attributeNames.clear();
+              attributeValues.clear();
+            }
+          }
+
+          attributes = "";
+          for(int i = 0; i < attributeNames.size(); i++){
+            attributes = attributes + " " + attributeNames.get(i) + ",";
+          }
+
+          values = "";
+          for(int i = 0; i < attributeValues.size(); i++){
+            values = values + " " + attributeValues.get(i) + ",";
+          }
+        }
+    }catch (FileNotFoundException e){
+        e.printStackTrace();
+    }
+  }
+
+  public static void oneFile(File XMLfile, String inputFileName){
+    try{
+      Scanner input = new Scanner(XMLfile);
+      String tablename = "";
+      String attributename = "";
+      String value = "";
+      boolean check = false;
+      ArrayList<String> attributeNames = new ArrayList<String>();
+      ArrayList<String> attributeValues = new ArrayList<String>();
+      ArrayList<String> tableNames = new ArrayList<String>();
+      int counter = 0;
+      String attributes = "";
+      String values = "";
+      PrintStream fileStream = new PrintStream(new FileOutputStream(inputFileName + ".txt", true));
+      // Create columns here with attributes
+      while(input.hasNextLine()){
+        tablename = "";
+        attributename = "";
+        value = "";
+        String text = input.nextLine();
         if(text.contains("<")){
-          String tablename = "";
-          String attributename = "";
-          String value = "";
+          tablename = "";
+          attributename = "";
+          value = "";
           int newIndex = 0;
           for(int i = (text.indexOf("<") +1); i < text.length(); i++){
             if((!(text.charAt(i) == ('>'))) && (!(text.charAt(i) == ('?')))){
@@ -153,7 +232,7 @@ public class XMLtoSQL{
                 attributename = tablename;
                 tablename = "";
                 for(int j = (text.indexOf(">") +1); j < text.length(); j++){
-                  if((!(text.charAt(j) == ('<'))) /*&& (!(text.charAt(j+1) == ('/')))*/){
+                  if((!(text.charAt(j) == ('<')))){
                     value = value + text.charAt(j);
                   }else{
                     break;
@@ -166,106 +245,41 @@ public class XMLtoSQL{
               break;
             }
           }
-  //        System.out.println("INSERT INTO " + XSDList.get(0).getTableName() + " () VALUES()")
-      //    System.out.println("check: " + check);
           if(check == true){
             counter++;
-    //        System.out.println("check: " + counter);
             attributeNames.add(attributename);
             attributeValues.add(value);
-          //  System.out.println("Table Name/ Object Name: " + XSDList.get(0).getTableName());
-          //  System.out.println("Attribute: " + attributename);
-          //  System.out.println("Value: " + value);
-          //  System.out.println("");
           }else{
             if(attributes.length() > 0){
               attributes = attributes.substring(1, attributes.length()-1);
             }
-        //    System.out.println(attributes);
 
             if(values.length() > 0){
               values = values.substring(1, values.length()-1);
             }
-        //    System.out.println(values);
+
             if(!(attributes.equals(""))){
-              System.out.println("INSERT INTO " + XSDList.get(0).getTableName() + " (" + attributes
-                + ") VALUES (" + values + ");");
+              fileStream.println("INSERT INTO " + tableNames.get(0) + " (" + attributes
+                  + ") VALUES (" + values + ");");
             }
 
             counter = 0;
+            tableNames.clear();
             attributeNames.clear();
             attributeValues.clear();
           }
-    //      if(!(attributename.equals(""))){
-    //        System.out.println("Table Name/ Object Name: " + XSDList.get(0).getTableName());
-    //        System.out.println("Attribute: " + attributename);
-    //        System.out.println("Value: " + value);
-    //        System.out.println("");
-    //      }
         }
         attributes = "";
         for(int i = 0; i < attributeNames.size(); i++){
           attributes = attributes + " " + attributeNames.get(i) + ",";
         }
 
-  //      attributes = attributes.trim();
-
-    //    System.out.println(attributes);
-
         values = "";
         for(int i = 0; i < attributeValues.size(); i++){
           values = values + " " + attributeValues.get(i) + ",";
         }
-//        values = values.trim();
-  //      System.out.println(values);
-      }
-    }catch (FileNotFoundException e){
-        e.printStackTrace();
-    }
-  }
-
-  public static void oneFile(File XMLfile, String inputFileName){
-    try{
-      Scanner input = new Scanner(XMLfile);
-
-      // Create columns here with attributes
-      while(input.hasNextLine()){
-        String text = input.nextLine();
-        if(text.contains("<")){
-          String tablename = "";
-          String attributename = "";
-          String value = "";
-          int newIndex = 0;
-          for(int i = (text.indexOf("<") +1); i < text.length(); i++){
-            if((!(text.charAt(i) == ('>'))) && (!(text.charAt(i) == ('?')))){
-              if(!(text.charAt(i) == ('/'))){
-                tablename = tablename + text.charAt(i);
-              }else{
-                break;
-              }
-            }else if((text.indexOf(">") + 1) < text.length()){
-              if((((text.charAt(text.indexOf(">") + 1)) >= 'a') && ((text.charAt(text.indexOf(">") + 1)) <= 'z'))
-                  || (((text.charAt(text.indexOf(">") + 1)) >= 'A') && ((text.charAt(text.indexOf(">") + 1)) <= 'Z'))
-                  || (((text.charAt(text.indexOf(">") + 1)) >= '0') && ((text.charAt(text.indexOf(">") + 1)) <= '9'))){
-                attributename = tablename;
-                tablename = "";
-                for(int j = (text.indexOf(">") +1); j < text.length(); j++){
-                  if((!(text.charAt(j) == ('<'))) /*&& (!(text.charAt(j+1) == ('/')))*/){
-                    value = value + text.charAt(j);
-                  }else{
-                    break;
-                  }
-                }
-              }
-              break;
-            }else{
-              break;
-            }
-          }
-          System.out.println("Table Name/ Object Name: " + tablename);
-          System.out.println("Attribute: " + attributename);
-          System.out.println("Value: " + value);
-          System.out.println("");
+        if(!(tablename.equals(""))){
+          tableNames.add(tablename);
         }
       }
     }catch (FileNotFoundException e){
