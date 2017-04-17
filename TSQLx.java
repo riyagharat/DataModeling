8683265c6129a24e3b291cf87374b33879f71f5d
TannerM504 <N00935492@ospreys.unf.edu>
2017-04-17 00:20:40 -0400

    Update TSQLx.java

--- TSQLx.java
+++ TSQLx.java
@@ -798,7 +798,7 @@ public class TSQLx{
              ColumnNumber = 0;
              while(!listOfTables.get(TableIndex).list.get(ColumnNumber).equals(ColumnName))//while we are in the wrong column, move
                 ColumnNumber++;
-	     if(Comparator.parseFloat() == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
+	     if(parseFloat(Comparator) == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
 	     	System.out.println("Error, incompatible type comparison");
 		return;
 	     }
@@ -849,7 +849,7 @@ public class TSQLx{
              ColumnNumber = 0;
              while(!listOfTables.get(TableIndex).list.get(ColumnNumber).equals(ColumnName))//while we are in the wrong column, move
                 ColumnNumber++;
-	     if(Comparator.parseFloat() == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
+	     if(parseFloat(Comparator) == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
 	     	System.out.println("Error, incompatible type comparison");
 		return;
 	     }
@@ -902,7 +902,7 @@ public class TSQLx{
              ColumnNumber = 0;
              while(!listOfTables.get(TableIndex).list.get(ColumnNumber).equals(ColumnName))//while we are in the wrong column, move
                 ColumnNumber++;
-	     if(Comparator.parseFloat() == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
+	     if(parseFloat(Comparator) == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
 	     	System.out.println("Error, incompatible type comparison");
 		return;
 	     }
@@ -951,7 +951,7 @@ public class TSQLx{
              ColumnNumber = 0;
              while(!listOfTables.get(TableIndex).list.get(ColumnNumber).equals(ColumnName))//while we are in the wrong column, move
                 ColumnNumber++;
-	     if(Comparator.parseFloat() == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
+	     if(parseFloat(Comparator) == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
 	     	System.out.println("Error, incompatible type comparison");
 		return;
 	     }
@@ -1000,7 +1000,7 @@ public class TSQLx{
              ColumnNumber = 0;
              while(!listOfTables.get(TableIndex).list.get(ColumnNumber).equals(ColumnName))//while we are in the wrong column, move
                 ColumnNumber++;
-   	     if(Comparator.parseFloat() == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
+   	     if(parseFloat(Comparator) == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
 	     	System.out.println("Error, incompatible type comparison");
 		return;
 	     }
@@ -1055,7 +1055,7 @@ public class TSQLx{
              ColumnNumber = 0;
              while(!listOfTables.get(TableIndex).list.get(ColumnNumber).equals(ColumnName))//while we are in the wrong column, move
                 ColumnNumber++;
-	     if(Comparator.parseFloat() == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
+	     if(parseFloat(Comparator) == true && listOfTables.get(TableIndex).list.get(ColumnNumber).getType().equalsIgnoreCase("Integer")) {
 	     	System.out.println("Error, incompatible type comparison");
 		return;
 	     }
